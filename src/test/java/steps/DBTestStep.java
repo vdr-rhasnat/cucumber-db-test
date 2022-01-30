1739d45ea3bcc6060877cd643f700824eaada6ce
Cetus Rayhan Hasnat2 <vdr_rhasnat2@parata.com>
2022-01-30 08:34:07 +0000

    Added sample DB test

--- src/test/java/steps/DBTestStep.java
+++ src/test/java/steps/DBTestStep.java
@@ -1,53 +1,62 @@
 package steps;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.xml.transform.Result;
+import java.sql.*;
 
 public class DBTestStep {
+    private Connection connection;
+    private String connectionStatus;
+
     @Given("Sql server connection")
     public void sqlServerConnection() {
-        /*String url = "jdbc:sqlserver://wvd-3;databaseName=Contact";
-        String user = "sa";
-        String password = "sa123";
-
-        try {
-            Connection connection = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected");
-        } catch(SQLException ex) {
-            System.out.println(ex.getMessage());
-        }*/
-
-        String connectionUrl =
-                "jdbc:sqlserver://localhost:1433;"
-                        + "database=Contact;"
-                        + "user=sa;"
-                        + "password=sa123;"
-                        + "encrypt=true;"
-                        + "trustServerCertificate=true;"
-                        + "loginTimeout=15;";
-
-        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
-            System.out.println("Connected");
-        }
-        // Handle any errors that may have occurred.
-        catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-        /*String connectionUrl = "jdbc:sqlserver://localhost;databaseName=Contact;user=sa;password=sa123";
+        String connectionUrl = "jdbc:sqlserver://localhost;databaseName=Contact;user=sa;password=sa123";
 
         try {
             // Load SQL Server JDBC driver and establish connection.
             System.out.print("Connecting to SQL Server ... ");
-            try (Connection connection = DriverManager.getConnection(connectionUrl)) {
-                System.out.println("Connected");
-            }
-        } catch (Exception e) {
+            connection = DriverManager.getConnection(connectionUrl);
+            System.out.println("Connected");
+            connectionStatus = "Success";
+
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
+            connectionStatus = "Error";
             //e.printStackTrace();
-        }*/
+        }
+    }
+
+    @Then("I should get connection status {string}")
+    public void iShouldGetConnectionStatus(String status) {
+        Assert.assertEquals(status, connectionStatus);
+    }
+
+    @And("Table {string} should exists")
+    public void tableShouldExists(String table) throws SQLException {
+        boolean isTableExists;
+        DatabaseMetaData dbm = connection.getMetaData();
+        ResultSet tables = dbm.getTables(null, null, table, null);
+        if (tables.next()) {
+            isTableExists = true;
+        }
+        else {
+            isTableExists = false;
+        }
+
+        Assert.assertTrue(isTableExists);
+    }
+
+    @And("I should get data from table")
+    public void iShouldGetDataFromTable() throws SQLException {
+        String sql = "select count(*) from Contacts";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+        Assert.assertNotEquals(0, count);
     }
 }
--- src/test/resources/features/dbtest.feature
+++ src/test/resources/features/dbtest.feature
@@ -1,3 +1,7 @@
 Feature: DB Test
   Scenario: Database Test
     Given Sql server connection
+    Then I should get connection status "Success"
+    And Table "Contacts" should exists
+    And I should get data from table
+
